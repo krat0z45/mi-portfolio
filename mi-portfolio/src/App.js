4e8f64f359d1f6e2f2a7e6f08e312b3826b026bd
diego <motokrat0z@gmail.com>
2025-06-07 20:37:04 -0600

    Primer commit

---
+++ mi-portfolio/src/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>¡Hola, soy Krat0z!</h1>
+        <p>Bienvenido a mi nuevo portfolio</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
