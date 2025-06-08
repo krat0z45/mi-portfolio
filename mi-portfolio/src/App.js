5a928d459364ca7b9902f4b37e8f0185a7365688
diego <motokrat0z@gmail.com>
2025-06-07 21:01:03 -0600

    Actualización: Cambié el título de la página

--- mi-portfolio/src/App.js
+++ mi-portfolio/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>¡Hola, soy Krat0z!</h1>
+        <h1>¡Hola, soy Krat0z!!</h1>
         <p>Bienvenido a mi nuevo portfolio</p>
       </header>
     </div>
